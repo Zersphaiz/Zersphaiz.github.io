e7c7fc40e4900f76c24ff87f1ee0f5c20169d724
Zersphaiz <96525372+Zersphaiz@users.noreply.github.com>
2023-06-06 22:25:24 +0300

    Update Gulbahar-Activity12.js

--- InclassActivities/Activity11/Gulbahar-Activity12.js
+++ InclassActivities/Activity11/Gulbahar-Activity12.js
@@ -1,24 +1,19 @@
-$(document).ready(function() {
-    // Click event handler for sidebar links
-    $("nav a").click(function(event) {
-        event.preventDefault(); // Prevent the default link behavior
+$(document).ready(function () {
+    $("nav a").click(function (event) {
+        event.preventDefault();
 
-        // Get the name of the JSON file from the link's title attribute
-        var jsonFileName = $(this).attr("title") + ".json";
+        var name_jsonFile = $(this).attr("title") + ".json";
 
-        // Clear the content in the main element
         $("main").empty();
 
-        // Retrieve JSON data using Ajax
-        $.getJSON(jsonFileName, function(data) {
-            // Create HTML elements for the speaker data
-            var speakerHTML = '<h1>' + data.speakers[0].title + '</h1>' +
-                              '<img src="' + data.speakers[0].image + '">' +
-                              '<h2>' + data.speakers[0].month + '<br>' + data.speakers[0].speaker + '</h2>' +
-                              '<p>' + data.speakers[0].text + '</p>';
+        $.getJSON(name_jsonFile, function (data) {
+            var html_speaker = 
+                '<h1>' + data.speakers[0].title + '</h1>' +
+                '<img src="' + data.speakers[0].image + '">' +
+                '<h2>' + data.speakers[0].month + '<br>' + data.speakers[0].speaker + '</h2>' +
+                '<p>' + data.speakers[0].text + '</p>';
 
-            // Append the speaker HTML to the main element
-            $("main").append(speakerHTML);
+            $("main").append(html_speaker);
         });
     });
 });
